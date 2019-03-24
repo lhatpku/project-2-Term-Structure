5f627ac426de570d83cd62e0cc584324c16d81db
Hao Lu <haolu@Haos-MacBook-Pro.local>
2019-03-23 19:02:17 -0500

    Enable JS to select date range and pick YTM

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,18 @@
 var yc_dropdown_list = [1,2,3,6,12,24,36,60,84,120,240,360];
 
+var form = document.querySelector('form');
+var yc_date_start = document.querySelector('#yc-date-start');
+var yc_date_end = document.querySelector('#yc-date-end');
+var yc_select = document.querySelector('#yc-select');
+
 function remove(array, element) {
   return array.filter(el => el !== element);
 }
+
+function unpack(rows, key) {
+  return rows.map(function(row) { return row[key]; });
+}
+
 
 function buildCharts() {
 
@@ -12,10 +22,6 @@ function buildCharts() {
 
   d3.json(url).then(function(yield_data){
   
-    function unpack(rows, key) {
-      return rows.map(function(row) { return row[key]; });
-    }
-
     var output_list = remove(Object.keys(yield_data[0]),'Date');
 
     colour.domain(output_list);
@@ -48,10 +54,80 @@ function buildCharts() {
     };
     
     Plotly.newPlot('chart', data, layout,{showSendToCloud: true});
+
+    // User Selection
+    form.addEventListener('submit',(e) => {
+
+      e.preventDefault();
     
+      var plot_date_start = yc_date_start.value;
+      var plot_date_end = yc_date_end.value;
+      var plot_yc_select = yc_select.options[yc_select.selectedIndex].value;
+
+      if (plot_date_start === "") {
+        plot_date_start = yield_data[0]["Date"];
+      }
+
+      if (plot_date_end === "") {
+        plot_date_end = yield_data[yield_data.length - 1]["Date"];
+      }
+
+      var startDate = new Date(plot_date_start);
+      var endDate = new Date(plot_date_end);
+
+      var yield_data_filter = yield_data.filter(function (d) {
+        var Date_obj = new Date(d.Date);
+        return (Date_obj >= startDate && Date_obj <= endDate);
+      });
+
+      if (plot_yc_select !== 'yc-all') {
+
+        data = [];
+
+        trace = {
+          type: "scatter",
+          mode: "lines",
+          name: plot_yc_select + ' - YTM',
+          x: unpack(yield_data_filter, 'Date'),
+          y: unpack(yield_data_filter, plot_yc_select),
+          line: {color: colour(plot_yc_select)}
+        };
+
+        data.push(trace);
+
+      }
+
+      else {
+
+        data = [];
+
+        output_list.forEach((key) => {
+
+          if (key !== "Date") {
+
+            trace = {
+              type: "scatter",
+              mode: "lines",
+              name: key + ' - YTM',
+              x: unpack(yield_data_filter, 'Date'),
+              y: unpack(yield_data_filter, key),
+              line: {color: colour(key)}
+            }
+
+            data.push(trace)
+      
+          }
+
+        });
+
+      }
+      
+      Plotly.newPlot('chart', data, layout,{showSendToCloud: true});
+
+    });
+       
   })
 }
 
-
-
 buildCharts();
+
